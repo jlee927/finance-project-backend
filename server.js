c8e8bb3256b7be9462c06c1fd6f473577fa54b2a
jlee927 <jlee927@uic.edu>
2025-04-08 18:10:03 -0500

    Changed server.js to troubleshoot heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 
 // external routes
-// const userRoutes = require("./routes/user");
+const userRoutes = require("./routes/user");
 // const revenueRoutes = require("./routes/revenue");
 // const expensesRoutes = require("./routes/expenses");
 // const savingsRoutes = require("./routes/savings");
