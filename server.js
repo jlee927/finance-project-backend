030e9c56de437d33f09f2e0c4d06209aa2fc2f1b
jlee927 <jlee927@uic.edu>
2025-04-08 17:57:37 -0500

    Changed server.js to troubleshoot heroku

--- server.js
+++ server.js
@@ -6,25 +6,25 @@ const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 
 // external routes
-const userRoutes = require("./routes/user");
-const revenueRoutes = require("./routes/revenue");
-const expensesRoutes = require("./routes/expenses");
-const savingsRoutes = require("./routes/savings");
-const spendingsRoutes = require("./routes/spending");
-const dashboardRoutes = require("./routes/dashboard");
-
-const mongoose = require("mongoose");
-const dbURI = process.env.MONGO_URI;
-app.use(cors());
-app.use(express.json());
-
-// routes
-app.use("/api/user", userRoutes);
-app.use("/data", revenueRoutes);
-app.use("/data", expensesRoutes);
-app.use("/data", savingsRoutes);
-app.use("/data", spendingsRoutes);
-app.use("/data", dashboardRoutes);
+// const userRoutes = require("./routes/user");
+// const revenueRoutes = require("./routes/revenue");
+// const expensesRoutes = require("./routes/expenses");
+// const savingsRoutes = require("./routes/savings");
+// const spendingsRoutes = require("./routes/spending");
+// const dashboardRoutes = require("./routes/dashboard");
+//
+// const mongoose = require("mongoose");
+// const dbURI = process.env.MONGO_URI;
+// app.use(cors());
+// app.use(express.json());
+//
+// // routes
+// app.use("/api/user", userRoutes);
+// app.use("/data", revenueRoutes);
+// app.use("/data", expensesRoutes);
+// app.use("/data", savingsRoutes);
+// app.use("/data", spendingsRoutes);
+// app.use("/data", dashboardRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello world");
